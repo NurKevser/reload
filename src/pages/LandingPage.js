b7e71522402752682c248b2f8846a0c0b4838495
NurKevser <nurkevserkadiroglu@gmail.com>
2023-07-19 13:04:31 +0300

    create structure

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "../styles/landingPage.module.scss";
+
+const LandingPage = () => {
+  return <div className={style.Container}>LandingPage</div>;
+};
+
+export default LandingPage;
